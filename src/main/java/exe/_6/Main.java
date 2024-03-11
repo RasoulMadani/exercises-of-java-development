164f0464dc265e938d5e847f416e6f0e0c7e659a
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-11 14:59:49 +0330

    add value to integer

---
+++ src/main/java/exe/_6/Main.java
@@ -0,0 +1,12 @@
+package exe._6;
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Number integer;
+        integer = 10;
+        System.out.println(integer);
+    }
+}
