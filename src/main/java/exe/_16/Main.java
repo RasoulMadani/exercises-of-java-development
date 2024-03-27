3ed71f9e12ebfe529a1e642fae152cbd351ae2cb
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-27 11:42:36 +0330

    سوال ۱۶

---
+++ src/main/java/exe/_16/Main.java
@@ -0,0 +1,13 @@
+package exe._16;
+
+
+/**
+ *
+ *  .1 برنامهای بنویسید که عبارت "World Hello "را در خروجی با استفاده از ثابتهای کاراکتری "t " \و"n " \و با فاصله مناسب )tab)
+ * و در دو خط جداگانه نمایش دهد. مثال:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello\tWorld\nHello\tWorld");
+    }
+}
