f40c5304a99097cc66d1ec2370484106307ef44b
Rasoulmadani <ali.rahbary@chmail.ir>
2024-03-27 13:43:34 +0330

    سوال 20

---
+++ src/main/java/exe/_20/Main.java
@@ -0,0 +1,20 @@
+package exe._20;
+
+
+/**
+ *
+ * .20 برنامهای بنویسید و چهار متغیر از نوع int، float، double، bool تعریف و مقداردهی کنید. سپس با استفاده از متدهای تبدیل
+ * نوع، مقادیر آنها را به رشته تبدیل کرده و در خروجی نمایش دهد.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        float f = 11.434F;
+        double d = 11.2;
+        boolean b = true;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+    }
+}
